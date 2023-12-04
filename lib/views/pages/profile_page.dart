a74a51ef5ebb334f9ad1d9af7644004844711d00
heyhey1028 <shoheiogawa1028@gmail.com>
2023-12-04 09:15:53 +0900

    chore: add loading

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,9 +9,10 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends State<ProfilePage> with SingleTickerProviderStateMixin {
   /// WebViewControllerオブジェクト
   late final WebViewController controller;
+  bool isLoading = true;
 
   /// 初期状態を設定
   @override
@@ -19,6 +21,18 @@ class _ProfilePageState extends State<ProfilePage> {
     controller = WebViewController()
       ..loadRequest(
         Uri.parse('https://docs.flutter.dev/dash#how-did-it-all-start'),
+      )
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (progress) async {
+            if (progress == 100) {
+              Future.delayed(
+                const Duration(seconds: 2),
+                () => setState(() => isLoading = false),
+              );
+            }
+          },
+        ),
       );
   }
 
@@ -26,7 +40,18 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: WebViewWidget(controller: controller),
+        child: isLoading
+            ? Center(
+                child: SizedBox.square(
+                  dimension: 200,
+                  child: LottieBuilder.asset(
+                    'lottie/flutter_loading.json',
+                    fit: BoxFit.cover,
+                    repeat: true,
+                  ),
+                ),
+              )
+            : WebViewWidget(controller: controller),
       ),
     );
   }
